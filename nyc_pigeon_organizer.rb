3343118f765def51806fdf1b4ded7b2b29e809dd
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-25 00:49:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
+  data.each_with_object({}) do |(key, value), array|
+    value.each do |inner_key, names|
+      binding.pry
+  end
   binding.pry
 end
