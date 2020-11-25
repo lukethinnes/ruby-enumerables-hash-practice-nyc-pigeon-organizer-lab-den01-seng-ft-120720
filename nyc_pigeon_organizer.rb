608e45d4efbc0b7830b335700c62437f00c78639
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-25 01:13:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  final_results=data.each_with_object({}) do |(key, value), array|
+    value.each do |inner_key, names|
+      names.each do |name|
+      if !array[name]
+         array[name] = {}
+      end
+      if !array[name][key]
+        !array[name][key] = []
+      end
+      array[name][key].push(inner_key.to_s)
+      end
+    end
+  end
+  final_results
 end
