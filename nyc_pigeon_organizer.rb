02119c75d05dde96b03f81d5b889174374eebd58
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-25 01:11:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,17 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
+
   final_results=data.each_with_object({}) do |(key, value), array|
     value.each do |inner_key, names|
       names.each do |name|
-      if !final_array[name]
-        final_array[name] = {}
+      if !array[name]
+         array[name] = {}
       end
-      if !final_array[name][key]
-        !final_array[name][key] = []
+      if !array[name][key]
+        !array[name][key] = []
       end
-      final_array[name][key].push(inner_key.to_s)
+      array[name][key].push(inner_key.to_s)
     end
     end
   end
